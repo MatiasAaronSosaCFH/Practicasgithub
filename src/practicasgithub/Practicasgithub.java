630f8bb407b5fad97b30924c05ff42ba84a23d40
Federico Lopez <federicolopez99@gmail.com>
2023-04-17 21:50:55 -0300

    v1

---
+++ src/practicasgithub/Practicasgithub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicasgithub;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practicasgithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    }
+    
+}
